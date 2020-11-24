909e4e5aa9430dfe44112ac5fab9b9d40ea43065
Murat_Satibaldiev <muratsatibaldiev@gmail.com>
2020-11-24 20:52:49 +0600

    fixed bugs

--- rundev-front.sh
+++ rundev-front.sh
@@ -4,5 +4,6 @@ export NODE_ENV=develop
 export PORT=3020
 export API_URL=https://neutrino-dev.elastoo.com/api/
 export API_URL_BROWSER=https://neutrino-dev.elastoo.com/api/
+export LANDING_BASE_URL=http://schutz.elastoo.com:1000
 
 cd frontend && yarn dev
