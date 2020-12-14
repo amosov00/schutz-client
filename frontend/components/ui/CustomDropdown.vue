95c8be0b96aa4fa407e4b821b01cc8f066a375d5
Marsel Melisov <marcel@elastoo.com>
2020-12-14 03:52:05 +0000

    add admin/users route add markup in users page add filter with search bar add users table

---
+++ frontend/components/ui/CustomDropdown.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+	b-dropdown.custom__dropdown(:disabled="disabled")
+		p.custom_dropdown__label(slot="trigger")
+			slot(name="label")
+		b-dropdown-item(
+			v-for="(button, index) in items"
+			aria-role="listitem"
+			:class="{ 'active': button.active }"
+			@click="button.action"
+			:key="index" ) {{ button.text }}
+
+</template>
+
+<script>
+export default {
+	props: {
+		items: {
+			type: Array,
+			required: true,
+		},
+
+		disabled: {
+			type: Boolean,
+			default: () => false,
+		}
+	},
+}
+</script>
+
+<style scoped>
+
+</style>
