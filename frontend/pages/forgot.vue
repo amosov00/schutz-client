5ea2f4c79192155ea07f71b23ec508547ab9e39a
Umar <mamatov.umar@mail.ru>
2020-12-17 19:53:44 +0500

    fix total investment and forgot page icon

--- frontend/pages/forgot.vue
+++ frontend/pages/forgot.vue
@@ -1,70 +1,86 @@
 <template>
-  <div>
-    <custom-slider :activeDot="1" :dots="1" next-page="/signup" prev-page="/signup">
-      <template slot="content">
-        <div class="columns is-fullheight">
-          <div class="column is-half is-flex flex-column">
-            <div class="is-size-5 mb-5">
-              Восстановление пароля
-            </div>
-            <div class="email-input">
-              <base-input
-                type="text"
-                size="6"
-                label="E-mail:"
-                v-model="email" />
-            </div>
-          </div>
-          <div class="column is-half is-flex is-flex-direction-column is-justify-content-space-between">
-            <img src="../static/login-page.png">
-            <custom-button @click.native="recover">Отправить</custom-button>
-          </div>
-        </div>
-      </template>
-    </custom-slider>
-  </div>
-  <!--section.hero-->
-  <!--div.hero-body-->
-  <!--h1.is-1.has-text-centered Recover-->
-  <!--div.card-->
-  <!--div.card-content-->
-  <!--b-field(:label="$t('email')")-->
-  <!--b-input.input-text-center(type="email" placeholder="your e-mail" v-model="email")-->
-  <!--b-button(type="is-info" :loading="loading" @click="recover") {{$t('send')}}-->
-  <!--nuxt-link(to="/signup") {{$t('registration')}}-->
+	<div>
+		<custom-slider
+			:activeDot="1"
+			:dots="1"
+			next-page="/signup"
+			prev-page="/signup"
+		>
+			<template slot="content">
+				<div class="columns is-fullheight">
+					<div class="column is-half is-flex flex-column">
+						<div class="is-size-5 mb-5">
+							Восстановление пароля
+						</div>
+						<div class="email-input">
+							<base-input
+								type="text"
+								size="6"
+								label="E-mail:"
+								v-model="email"
+							/>
+						</div>
+					</div>
+					<div
+						class="column is-half is-flex is-flex-direction-column is-justify-content-space-between"
+					>
+						<div class="auth-image">
+							<img src="/login_success.svg" />
+						</div>
+						<custom-button @click.native="recover">Отправить</custom-button>
+					</div>
+				</div>
+			</template>
+		</custom-slider>
+	</div>
+	<!--section.hero-->
+	<!--div.hero-body-->
+	<!--h1.is-1.has-text-centered Recover-->
+	<!--div.card-->
+	<!--div.card-content-->
+	<!--b-field(:label="$t('email')")-->
+	<!--b-input.input-text-center(type="email" placeholder="your e-mail" v-model="email")-->
+	<!--b-button(type="is-info" :loading="loading" @click="recover") {{$t('send')}}-->
+	<!--nuxt-link(to="/signup") {{$t('registration')}}-->
 </template>
 
 <script>
 export default {
-  name: 'login',
-  layout: 'auth',
-  component: {},
-  data() {
-    return {
-      email: '',
-      loading: false
-    }
-  },
-  methods: {
-    async recover() {
-      this.loading = true
+	name: "login",
+	layout: "auth",
+	component: {},
+	data() {
+		return {
+			email: "",
+			loading: false
+		};
+	},
+	methods: {
+		async recover() {
+			this.loading = true;
 
-      if (await this.$store.dispatch('startRecover', { email: this.email })) {
-        this.$buefy.toast.open({ message: this.$i18n.t('authRecoverSuccess'), type: 'is-primary' })
-        this.email = ''
-        this.$nuxt.$router.replace({ path: '/' })
-      } else {
-        this.$buefy.toast.open({ message: this.$i18n.t('authRecoverError'), type: 'is-danger' })
-      }
+			if (await this.$store.dispatch("startRecover", { email: this.email })) {
+				this.$buefy.toast.open({
+					message: this.$i18n.t("authRecoverSuccess"),
+					type: "is-primary"
+				});
+				this.email = "";
+				this.$nuxt.$router.replace({ path: "/" });
+			} else {
+				this.$buefy.toast.open({
+					message: this.$i18n.t("authRecoverError"),
+					type: "is-danger"
+				});
+			}
 
-      this.loading = false
-    }
-  }
-}
+			this.loading = false;
+		}
+	}
+};
 </script>
 
 <style lang="scss" scoped>
 .email-input {
-  margin-top: 120px;
+	margin-top: 120px;
 }
 </style>
